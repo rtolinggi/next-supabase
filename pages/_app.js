cf7bcdb376a386ceaaa9f7977a416384db091987
Rio Tolinggi <lovelytolinggi@gmail.com>
2022-04-24 12:35:13 +0800

    first deploy to Netlify

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,10 @@
 import { ThemeProvider } from "styled-components";
 import theme from "../styles/theme";
-import GlobalStyle from "../styles/globals .style";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider theme={theme}>
-      <GlobalStyle />
       <Component {...pageProps} />
     </ThemeProvider>
   );
