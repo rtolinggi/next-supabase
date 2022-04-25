8274b8fde613393cfb029f03b3a2a1c547f314e7
rtolinggi <86676841+rtolinggi@users.noreply.github.com>
2022-04-25 10:39:40 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
   compiler: {
     // ssr and displayName are configured by default
     styledComponents: true,
